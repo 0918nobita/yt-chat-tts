858c89b83ffa87d8a3215366ea79b20d0adc17b9
0918nobita <nobita.0918@gmail.com>
2023-03-05 19:22:18 +0900

    Do refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,41 @@
-pub mod sound_player;
-pub mod synthesis;
+mod sound_player;
+mod synthesis;
+
+use actix::Actor;
+use sound_player::{SoundPlayerActor, SoundPlayerRequest};
+use synthesis::{SynthesisActor, SynthesisRequest};
+
+pub fn event_loop(
+    output_stream: rodio::OutputStream,
+    output_stream_handle: rodio::OutputStreamHandle,
+    sink: rodio::Sink,
+) -> std::io::Result<()> {
+    let system = actix::System::new();
+
+    system.block_on(async {
+        let synthesis_addr = SynthesisActor.start();
+
+        let sound_player_addr = (SoundPlayerActor {
+            output_stream,
+            output_stream_handle,
+            sink,
+        })
+        .start();
+
+        let wav = synthesis_addr
+            .send(SynthesisRequest {
+                text: "こんにちは".to_owned(),
+            })
+            .await
+            .expect("Failed to send message to synthesis actor")
+            .expect("Failed to synthesize sound");
+
+        sound_player_addr
+            .send(SoundPlayerRequest { wav })
+            .await
+            .expect("Failed to send message to sound_player actor")
+            .expect("Failed to play sound");
+    });
+
+    system.run()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,14 @@
-use actix::Actor;
 use anyhow::Context;
-use yt_chat_tts::{
-    sound_player::{SoundPlayerActor, SoundPlayerRequest},
-    synthesis::{SynthesisActor, SynthesisRequest},
-};
+use yt_chat_tts::event_loop;
 
 fn main() -> anyhow::Result<()> {
     let (output_stream, output_stream_handle) = rodio::OutputStream::try_default()?;
 
     let sink = rodio::Sink::try_new(&output_stream_handle)?;
 
-    let system = actix::System::new();
-
-    system.block_on(async {
-        let synthesis_addr = SynthesisActor.start();
-
-        let sound_player_addr = (SoundPlayerActor {
-            output_stream,
-            output_stream_handle,
-            sink,
-        })
-        .start();
-
-        let wav = synthesis_addr
-            .send(SynthesisRequest {
-                text: "こんにちは".to_owned(),
-            })
-            .await
-            .expect("Failed to send message")
-            .expect("Failed to synthesize sound");
-
-        sound_player_addr
-            .send(SoundPlayerRequest { wav })
-            .await
-            .expect("Failed to send message to sound_player actor")
-            .expect("Failed to play sound");
-    });
-
-    system.run().context("Actix system stopped")
+    event_loop(
+        output_stream,
+        output_stream_handle,
+        sink
+    ).context("Event loop stopped unexpectedly")
 }
