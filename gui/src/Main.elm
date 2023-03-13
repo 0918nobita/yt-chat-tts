900104740ea9f6bcd6f6e5c27c58b3dac347e549
0918nobita <nobita.0918@gmail.com>
2023-03-13 14:55:18 +0900

    URL から GET パラメータを取り出すサンプルを追加した

--- gui/src/Main.elm
+++ gui/src/Main.elm
@@ -2,8 +2,11 @@ port module Main exposing (main)
 
 import Browser
 import Html exposing (Html, button, div, input, text)
-import Html.Attributes exposing (placeholder, value)
+import Html.Attributes exposing (classList, placeholder, value)
 import Html.Events exposing (onClick, onInput)
+import Url
+import Url.Parser exposing ((</>), s)
+import Url.Parser.Query
 
 
 main : Program () Model Msg
@@ -24,14 +27,18 @@ port messageReceiver : (String -> msg) -> Sub msg
 
 type alias Model =
     { yourName : String
-    , message : Maybe String
+    , message : String
+    , url : String
+    , urlParseResult : String
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( { yourName = ""
-      , message = Nothing
+      , message = ""
+      , url = ""
+      , urlParseResult = ""
       }
     , Cmd.none
     )
@@ -39,8 +46,20 @@ init _ =
 
 type Msg
     = YourNameChanged String
+    | UrlChanged String
     | Send
     | Recv String
+
+
+parseVideoId : String -> Maybe String
+parseVideoId =
+    let
+        v =
+            Url.Parser.Query.string "v"
+    in
+    Url.fromString
+        >> Maybe.andThen (Url.Parser.parse (s "watch" </> Url.Parser.query v))
+        >> Maybe.withDefault Nothing
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -51,11 +70,29 @@ update msg model =
             , Cmd.none
             )
 
+        UrlChanged newUrl ->
+            let
+                urlParseResult =
+                    if String.isEmpty newUrl then
+                        ""
+
+                    else
+                        parseVideoId newUrl
+                            |> Maybe.map ((++) "Video ID: ")
+                            |> Maybe.withDefault "Faild to parse video id from URL"
+            in
+            ( { model
+                | url = newUrl
+                , urlParseResult = urlParseResult
+              }
+            , Cmd.none
+            )
+
         Send ->
             ( model, greet model.yourName )
 
         Recv message ->
-            ( { model | message = Just message }
+            ( { model | message = message }
             , Cmd.none
             )
 
@@ -73,9 +110,19 @@ view model =
                 [ placeholder "Type your name"
                 , value model.yourName
                 , onInput YourNameChanged
+                , classList []
                 ]
                 []
             , button [ onClick Send ] [ text "Greet" ]
             ]
-        , div [] [ text (Maybe.withDefault "" model.message) ]
+        , div [] [ text model.message ]
+        , div []
+            [ input
+                [ placeholder "Type URL"
+                , value model.url
+                , onInput UrlChanged
+                ]
+                []
+            ]
+        , div [] [ text model.urlParseResult ]
         ]
